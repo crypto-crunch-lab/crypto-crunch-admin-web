3ec0b20d77585b6cee8b8e022a1ada5c6dd87220
ywoo-park2 <ywoo.park2@navercorp.com>
2022-05-17 17:57:15 +0900

    feat: 디파이 플랫폼 페이지 구현

---
+++ pages/api/defi/platforms/update/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<boolean>) {
+  if (req.method === 'POST') {
+    const { data } = await axios.post('http://server.crypto-crunch-tech.com:8080/api/v1/defi/admin/platforms/update', req.body)
+    res.status(200).json(data)
+  }
+}
